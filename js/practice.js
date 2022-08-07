a4e2671979e3a3157465918dbe1fc7e8a0ef063b
Fevillagra <ezefac.95@gmail.com>
2022-08-07 00:18:42 -0300

    dom parcialmente terminado

--- js/practice.js
+++ js/practice.js
@@ -2,16 +2,34 @@
 //no sea igual a 3 se imprime el valor del arrayB con el indice "array", en caso de que
 //si sea igual a 3 se recorre el arrayA y se imprime cada uno de los valores.
 
-let arrayA = ['d', 'e', 'f'];
-let arrayB = ['a', 'b', 'c', arrayA];
+// let arrayA = ['d', 'e', 'f'];
+// let arrayB = ['a', 'b', 'c', arrayA];
 
-for( let array in arrayB) {
-    if(array == 3) {
-        for(let arrays of arrayA) {
-            console.log(arrays);
-        }
-    } else {
-        console.log(arrayB[array]);
-    }
-}
+// for( let array in arrayB) {
+//     if(array == 3) {
+//         for(let arrays of arrayA) {
+//             console.log(arrays);
+//         }
+//     } else {
+//         console.log(arrayB[array]);
+//     }
+// }
 
+let container = document.querySelector('.container');
+
+// let item = document.createElement('LI');
+
+// const textItem = document.createTextNode('ESTO ES UN ITEM');
+
+// item.appendChild(textItem);
+
+// container.appendChild(item);
+
+const fragment = document.createDocumentFragment();
+
+const item = document.createElement('P');
+
+item.textContent = 'HOLA MUNDO!!!';
+
+fragment.appendChild(item);
+container.appendChild(fragment);
